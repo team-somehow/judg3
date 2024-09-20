056c3de87eb3a07c3cae3d98ddc83569c9b46212
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2024-09-21 07:18:07 +0800

    feat: complete auth integration in frontend

--- frontend/src/constants/index.tsx
+++ frontend/src/constants/index.tsx
@@ -1,2 +1,1 @@
-export const WORLDCOIN_VERIFICATION_ROUTE =
-  import.meta.env.VITE_BACKEND_BASE_URL + "api/verify";
+export const WORLDCOIN_VERIFICATION_ROUTE = "http://127.0.0.1:8000/auth/login/";
