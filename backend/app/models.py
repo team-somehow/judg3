ff7150c5d0cbc70a595e821a78da987cbf8a847e
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2024-09-20 22:43:33 +0800

    feat(backend): event project models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
+# auth
+
 
 class UserManager(BaseUserManager):
     def create_user(self, world_id_user_hash):
@@ -25,3 +27,30 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'world_id_user_hash'
+
+# events
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    photo = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, default='Active')
+
+    def __str__(self):
+        return self.name
+
+
+# projects
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.URLField()
+    photo = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,12 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+APP_ID = env('WORLDCOIN_APP_ID')
+ACTION_ID = env('WORLDCOIN_ACTION_ID')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -130,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
