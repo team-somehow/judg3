1bfb453d80b5c08ef6806988abf34b32bdcb87de
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2024-09-21 03:53:20 +0800

    feat: backend get events no auth and auth

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path("", hello.index, name="index"),
 
     path("auth/login/", auth.verify_world_id, name="login"),
+
+    # event
     path('event/', events.create_event, name='create_event'),
+    path('get-event/', events.get_all_events_with_status, name='get_event'),
+    path('get-event-noauth/', events.get_all_events_noauth, name='get_event'),
+
     path('vote/', vote.create_vote, name="create_vote"),
     path('leaderboard/', leaderboard.get_leaderboard, name="get_leaderboard")
 ]
--- backend/app/views/events.py
+++ backend/app/views/events.py
@@ -3,12 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
-from app.models import Event
+from app.models import Event, Application
+
 
 class EventCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'name', 'description', 'photo'] 
+        fields = ['id', 'name', 'description', 'photo']
 
 
 @api_view(['POST'])
@@ -21,3 +22,34 @@ def create_event(request):
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET'])
+def get_all_events_noauth(request):
+    events = Event.objects.all()
+    serializer = EventCreateSerializer(
+        events, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_all_events_with_status(request):
+    user = request.user  # Get the authenticated user
+    events = Event.objects.all()  # Get all events
+
+    events_data = []
+
+    for event in events:
+        # Check if the user has applied to this event
+        is_applied = Application.objects.filter(
+            user=user, event=event).exists()
+        status1 = "applied" if is_applied else "not_applied"
+
+        # Serialize the event
+        event_serializer = EventCreateSerializer(event)
+        event_data = event_serializer.data
+        event_data['status'] = status1  # Add the application status
+
+        events_data.append(event_data)
+
+    return Response(events_data, status=status.HTTP_200_OK)
