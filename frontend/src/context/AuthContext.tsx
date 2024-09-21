df970f51da6196f98056cd9b4e6594ea3dc9c89b
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2024-09-21 15:54:06 +0800

    feat: auth magic and dynamic compete

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { useMagic } from '../components/auth/magic/MagicContext';
-import Loading from '../components/ui/Loading';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useMagic } from "../components/auth/magic/MagicContext";
+import Loading from "../components/ui/Loading";
+import { useDynamicWallet } from "../components/auth/dynamic/dynamicHooks";
 
 type Props = {
   children: React.ReactNode;
@@ -10,51 +11,73 @@ export type AuthContextType = {
   setToken: (token: string) => void;
   address: string | null;
   setAddress: (address: string) => void;
+
+  currentAuthSupply: string;
 };
 export const AuthContext = createContext<AuthContextType>({
   token: null,
   setToken: () => {},
   address: null,
   setAddress: () => {},
+  currentAuthSupply: "",
 });
 
 // eslint-disable-next-line react-refresh/only-export-components,
 export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }: Props) => {
+  const [currentAuthSupply, setCurrentSupply] = useState<"magic" | "dynamic">(
+    "dynamic"
+  );
+
   const [token, setToken] = useState<string | null>(null);
   const [address, setAddress] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   const { magic } = useMagic();
+  const { address: dynamicAddress } = useDynamicWallet();
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (!magic) return console.error('Magic not initialized');
-      try {
-        console.log('Fetching user metadata');
+    if (currentAuthSupply === "magic") {
+      const fetchData = async () => {
+        if (!magic) return console.error("Magic not initialized");
+        try {
+          console.log("Fetching user metadata");
 
-        setLoading(true);
-        const m = await magic.user.getMetadata();
-        setAddress(m.publicAddress!);
-        setToken(localStorage.getItem('token')!);
+          setLoading(true);
+          const m = await magic.user.getMetadata();
+          setAddress(m.publicAddress!);
+          setToken(localStorage.getItem("token")!);
 
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
+          setLoading(false);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchData();
+    }
   }, [magic, setAddress, setToken]);
+
+  useEffect(() => {
+    if (currentAuthSupply === "dynamic") {
+      setLoading(true);
+      if (!dynamicAddress) return;
+
+      setAddress(dynamicAddress);
+      setLoading(false);
+    }
+  }, [dynamicAddress, setAddress]);
 
   if (loading) {
     return <Loading loading={true} />;
   }
 
   return (
-    <AuthContext.Provider value={{ token, setToken, setAddress, address }}>
+    <AuthContext.Provider
+      value={{ token, setToken, setAddress, address, currentAuthSupply }}
+    >
       {children}
     </AuthContext.Provider>
   );
