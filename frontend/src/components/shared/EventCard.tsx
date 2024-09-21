fe7d3560f146ae5417abd0b3f4160d3ba6e79e44
Pargat-Dhanjal <pargat14@hotmail.com>
2024-09-22 03:51:37 +0800

    feat: hide home screen voting button

--- frontend/src/components/shared/EventCard.tsx
+++ frontend/src/components/shared/EventCard.tsx
@@ -1,23 +1,24 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Typography from "@mui/material/Typography";
-import { Avatar, Button } from "@mui/material";
-import { useAuth } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-import axiosInstance from "../../config/axios"; // Import your axios instance
-import useMorphInteractions from "../../hooks/morph/useMorphInteractions";
-import { useFlowInteraction } from "../../hooks/useFlowInteraction";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import { Avatar, Button } from '@mui/material';
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../config/axios'; // Import your axios instance
+import useMorphInteractions from '../../hooks/morph/useMorphInteractions';
+import { useFlowInteraction } from '../../hooks/useFlowInteraction';
 
 interface EventCardProps {
   id: number;
   name: string;
   description: string;
   photo: string;
-  blockchain_event_id: string;
+  blockchain_event_id?: string;
+  hideButton?: boolean;
 }
 
 const EventCard: React.FC<EventCardProps> = ({
@@ -26,6 +27,7 @@ const EventCard: React.FC<EventCardProps> = ({
   description,
   photo,
   blockchain_event_id,
+  hideButton = false,
 }) => {
   const { token } = useAuth();
   const navigate = useNavigate();
@@ -34,33 +36,33 @@ const EventCard: React.FC<EventCardProps> = ({
   const { currentAuthSupply } = useAuth();
 
   const handleButtonClick = async (eventId: number) => {
-    console.log("Button clicked", eventId);
+    console.log('Button clicked', eventId);
 
     if (token) {
       // If the user is authenticated, apply to the event
       try {
-        const response = await axiosInstance.post("/voter-apply-event/", {
+        const response = await axiosInstance.post('/voter-apply-event/', {
           event_id: eventId,
         });
-        console.log("Application successful:", response.data);
+        console.log('Application successful:', response.data);
         {
-          currentAuthSupply === "magic" &&
+          currentAuthSupply === 'magic' &&
             applyVoter({ eventId: blockchain_event_id.toString() });
         }
         {
-          currentAuthSupply === "dynamic" &&
+          currentAuthSupply === 'dynamic' &&
             handleAddVoterToEvent({ eventId: blockchain_event_id.toString() });
         }
 
         // Navigate to the voting dashboard if the application was successful
         navigate(`/voter-dashboard/?tab=applied`);
       } catch (error) {
-        console.error("Error applying to event:", error);
+        console.error('Error applying to event:', error);
         // Handle any errors, such as showing a message to the user
       }
     } else {
       // If not authenticated, redirect to the login page
-      navigate("/login");
+      navigate('/login');
     }
   };
 
@@ -68,9 +70,9 @@ const EventCard: React.FC<EventCardProps> = ({
     <Card
       sx={{
         maxWidth: 345,
-        textAlign: "start",
+        textAlign: 'start',
         minWidth: 345,
-        height: "400px",
+        height: hideButton ? '340px' : '400px',
       }}
     >
       <CardHeader
@@ -78,7 +80,7 @@ const EventCard: React.FC<EventCardProps> = ({
           <Avatar
             aria-label="logo"
             sx={{
-              bgcolor: "primary.main",
+              bgcolor: 'primary.main',
               width: 40,
               height: 40,
             }}
@@ -88,32 +90,34 @@ const EventCard: React.FC<EventCardProps> = ({
         }
         title={name}
       />
-      <CardMedia component="img" height="180" image={photo} alt={"Image"} />
+      <CardMedia component="img" height="180" image={photo} alt={'Image'} />
       <CardContent sx={{ p: 1 }}>
         <Typography
           variant="subtitle2"
           sx={{
-            color: "text.secondary",
-            whiteSpace: "pre-wrap",
-            height: "80px",
-            overflow: "auto",
-            borderRadius: "5px",
+            color: 'text.secondary',
+            whiteSpace: 'pre-wrap',
+            height: '80px',
+            overflow: 'auto',
+            borderRadius: '5px',
           }}
         >
           {description}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <Button
-          variant="outlined"
-          fullWidth
-          onClick={() => {
-            handleButtonClick(id);
-          }}
-        >
-          Apply to Event
-        </Button>
-      </CardActions>
+      {!hideButton && (
+        <CardActions disableSpacing>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={() => {
+              handleButtonClick(id);
+            }}
+          >
+            Apply to Event
+          </Button>
+        </CardActions>
+      )}
     </Card>
   );
 };
